1416af6435ec06350161a8c164a233928db6f779
JR Ladd <johnrobertladd@gmail.com>
2017-08-28 23:14:15 -0400

    drag and drop file, ranking number

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 $( function() {
 
+  $('textarea').on('dragover', function(e) {
+    e.preventDefault(e);
+    e.stopPropagation(e);
+  });
+
+  $('textarea').on('drop', function(e) {
+    e.preventDefault(e);
+    e.stopPropagation(e);
+    $textarea = this;
+    var files = e.originalEvent.dataTransfer.files;
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      $textarea.value = e.target.result;
+    }
+    for (var i=0;i<files.length;i++) {
+      reader.readAsText(files[i]);
+    }
+  });
+
   $('.btn').click(function(){
     var $btn = $(this).button('loading');
     setTimeout(function() {
@@ -18,8 +37,6 @@ $( function() {
       // console.log(weighted_edges);
       var G = new jsnx.Graph();
       G.addWeightedEdgesFrom(weighted_edges);
-      // console.log(G.nodes());
-      // console.log(G.edges());
       var betweenness = jsnx.betweennessCentrality(G)._stringValues;
       var clustering = jsnx.clustering(G)._stringValues;
       var degree = G.degree()._stringValues;
@@ -28,13 +45,17 @@ $( function() {
       var averageClustering = jsnx.averageClustering(G);
       var transitivity = jsnx.transitivity(G);
       var info = jsnx.info(G);
+      var degreeSorted = reverse_sort(degree);
+      var betweennessSorted = reverse_sort(betweenness);
+      var eigenvectorSorted = reverse_sort(eigenvector);
+      var clusteringSorted = reverse_sort(clustering);
       G.nodes().forEach(function(node){
         var row = '<tr>';
         row = row + '<td>' + node + '</td>'
-        row = row + '<td>' + degree[node] + '</td>';
-        row = row + '<td>' + betweenness[node].toFixed(10) + '</td>';
-        row = row + '<td>' + eigenvector[node].toFixed(10) + '</td>';
-        row = row + '<td>' + clustering[node].toFixed(10) + '</td>';
+        row = row + '<td>' + degree[node] + /*' (rank: ' + degreeSorted.indexOf(node).toString() + ')*/'</td>';
+        row = row + '<td>' + betweenness[node].toFixed(10) + ' (' + betweennessSorted.indexOf(node).toString() + ')</td>';
+        row = row + '<td>' + eigenvector[node].toFixed(10) + ' (' + eigenvectorSorted.indexOf(node).toString() + ')</td>';
+        row = row + '<td>' + clustering[node].toFixed(10) + ' (' + clusteringSorted.indexOf(node).toString() + ')</td>';
         rows = rows + row
       });
       $('tbody').append(rows);
@@ -53,3 +74,7 @@ $( function() {
 
 
 });
+
+function reverse_sort(dict) {
+  return Object.keys(dict).sort(function(a,b) {return dict[b]-dict[a]});
+}
